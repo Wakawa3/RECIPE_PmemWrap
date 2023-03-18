b956ba0d863436b355c1825c5a19ac2734cf54f7
Nakashima <satoshinakashima@sslab.ics.keio.ac.jp>
2023-03-18 07:21:40 +0000

    pmemwrap

--- P-CLHT/testexamples.sh
+++ P-CLHT/testexamples.sh
@@ -33,33 +33,32 @@ OUTPUT_TEXT=${OUT_LOC}/${BIN}_${TEST_MEMCPY_TYPE}_output.txt
 ABORT_TEXT=${OUT_LOC}/${BIN}_${TEST_MEMCPY_TYPE}_abort.txt
 ERROR_TEXT=${OUT_LOC}/${BIN}_${TEST_MEMCPY_TYPE}_error.txt
 
-
-echo "" > ${OUT_LOC}/${BIN}_output.txt
-echo "" > ${OUT_LOC}/${BIN}_abort.txt
-echo "" > ${OUT_LOC}/${BIN}_error.txt
+echo "" > ${OUTPUT_TEXT}
+echo "" > ${ABORT_TEXT}
+echo "" > ${ERROR_TEXT}
 # echo "" > ${OUT_LOC}/${BIN}_memcpy.txt
 
 for i in `seq 100`
 do
-    echo "${i}" >> ${OUT_LOC}/${BIN}_output.txt
-    echo "${i}" >> ${OUT_LOC}/${BIN}_abort.txt
-    echo "${i}" >> ${OUT_LOC}/${BIN}_error.txt
+    echo "${i}" >> ${OUTPUT_TEXT}
+    echo "${i}" >> ${ABORT_TEXT}
+    echo "${i}" >> ${ERROR_TEXT}
     export PMEMWRAP_ABORT=1
     export PMEMWRAP_SEED=${i}
-    export PMEMWRAP_MEMCPY=NORMAL_MEMCPY
-    ${TEST_ROOT}/build/${BIN} 10000 1 >> ${OUT_LOC}/${BIN}_output.txt 2>> ${OUT_LOC}/${BIN}_abort.txt
+    export PMEMWRAP_MEMCPY=${TEST_MEMCPY_TYPE}
+    ${TEST_ROOT}/build/${BIN} 10000 1 >> ${OUTPUT_TEXT} 2>> ${ABORT_TEXT}
     ${PMEMWRAP_ROOT}/PmemWrap_memcpy.out ${PMIMAGE} ${COPYFILE}
 #  >> ${OUT_LOC}/${BIN}_memcpy.txt
 
     export PMEMWRAP_ABORT=0
     export PMEMWRAP_MEMCPY=NO_MEMCPY
-    timeout -k 1 40 bash -c "${TEST_ROOT}/build/${BIN} 10000 1 >> ${OUT_LOC}/${BIN}_output.txt 2>> ${OUT_LOC}/${BIN}_error.txt" 2>>${OUT_LOC}/${BIN}_abort.txt
-    echo "timeout $?" >> ${OUT_LOC}/${BIN}_abort.txt
+    timeout -k 1 40 bash -c "${TEST_ROOT}/build/${BIN} 10000 1 >> ${OUTPUT_TEXT} 2>> ${ERROR_TEXT}" 2>>${ABORT_TEXT}
+    echo "timeout $?" >> ${ABORT_TEXT}
     rm ${PMIMAGE} ${COPYFILE}
     
-    echo "" >> ${OUT_LOC}/${BIN}_output.txt
-    echo "" >> ${OUT_LOC}/${BIN}_abort.txt
-    echo "" >> ${OUT_LOC}/${BIN}_error.txt
+    echo "" >> ${OUTPUT_TEXT}
+    echo "" >> ${ABORT_TEXT}
+    echo "" >> ${ERROR_TEXT}
 done
 
 #rm ${TEST_ROOT}/include/libpmemobj.h
